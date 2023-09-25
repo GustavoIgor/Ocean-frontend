3f7ddf95d7bd8e5744b8e81b5872499f95ed7ec9
Gustavo Boer <56308069+GustavoIgor@users.noreply.github.com>
2023-09-25 23:48:27 +0000

    Criação de tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <div className="card">
         <h2>Mandaçaia</h2>
+        <div className="card__tags">
+          <span className="tag">Nome Cientifico: Melipona quadrifasciata</span>
+          <span className="tag">A</span>
+          
+        </div>
         <img src='https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg'/>
       </div>
     </>
