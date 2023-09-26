b6fa61c624e62bec811973ecd15a9914f10c86be
Gustavo Boer <56308069+GustavoIgor@users.noreply.github.com>
2023-09-26 00:03:15 +0000

    Conteudo css e html movido para componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Mandaçaia</h2>
-        <div className="card__tags">
-          <span className="tag">Nome Cientifico: Melipona quadrifasciata</span>
-          <span className="tag">A</span>
-          
-        </div>
-        <img src='https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg'/>
-      </div>
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import "./Card.css"
+
+export default function Card() {
+    return <div className="card">
+    <h2>Mandaçaia</h2>
+    <div className="card__tags">
+      <span className="tag">Nome Cientifico: Melipona quadrifasciata</span>
+    </div>
+    <img src='https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg'/>
+  </div>
+}
