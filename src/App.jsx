46fe26e1c31bb2d722666639c7393af4dd584724
Gustavo Boer <56308069+GustavoIgor@users.noreply.github.com>
2023-09-26 01:34:41 +0000

    map

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    nome: 'Mandaçaia',
+    imagemUrl: 'https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg',
+    tags: ['tag1', 'tag2']
+  }
+  const item2 = {
+    nome: 'Jataí',
+    imagemUrl: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*_Fp1zZQDjHoI_46WL-N5xQ.jpeg',
+    tags: ['tag1', 'tag2']
+  }
+
+  const item3 = {
+    nome: 'Guaraipo',
+    imagemUrl: 'https://meliponariofarroupilha.com.br/wp-content/uploads/2022/04/Guaraipo-768x384.png',
+    tags: ['tag1', 'tag2']
+  }
+
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <Card />
+      {itens.map(function (item) {
+        return <Card item = {item}/>
+      })}
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
 import "./Card.css"
 
-export default function Card() {
+export default function Card(props) {
+    const item = props.item;
     return <div className="card">
-    <h2>Mandaçaia</h2>
+    <h2>{item.nome}</h2>
     <div className="card__tags">
       <span className="tag">Nome Cientifico: Melipona quadrifasciata</span>
     </div>
-    <img src='https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg'/>
+    <img src={item.imagemUrl}/>
   </div>
 }
