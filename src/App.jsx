58d4c957da535180d6f02f3e61f2fbc30c972afc
Gustavo Boer <56308069+GustavoIgor@users.noreply.github.com>
2023-09-27 23:48:26 +0000

    Tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ function App() {
   const item1 = {
     nome: 'Mandaçaia',
     imagemUrl: 'https://mandacaiasocial.com.br/wp-content/uploads/2021/09/abelha-mandacaia-01.jpg',
-    tags: ['tag1', 'tag2']
+    tags: ['Melipona quadrifasciata']
   }
   const item2 = {
     nome: 'Jataí',
     imagemUrl: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*_Fp1zZQDjHoI_46WL-N5xQ.jpeg',
-    tags: ['tag1', 'tag2']
+    tags: ['Tetragonisca angustula']
   }
 
   const item3 = {
     nome: 'Guaraipo',
     imagemUrl: 'https://meliponariofarroupilha.com.br/wp-content/uploads/2022/04/Guaraipo-768x384.png',
-    tags: ['tag1', 'tag2']
+    tags: ['Melipona bicolor schencki']
   }
 
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,30 @@
+import Tag from "../Tag/Tag";
 import "./Card.css"
 
 export default function Card(props) {
-    const item = props.item;
-    return <div className="card">
+  const item = props.item;
+
+  // Garantir que temos sempre um array em tags
+
+  // Jeito literal
+  // let tags = item.tags;
+
+  // if (!tags) {
+  //   tags = [];
+  // }
+
+  // Jeito otimizado
+  const tags = item.tags || [];
+
+  return <div className="card">
     <h2>{item.nome}</h2>
-    <div className="card__tags">
-      <span className="tag">Nome Cientifico: Melipona quadrifasciata</span>
+
+    <div className="tags">
+      {tags.map(function (tag, index) {
+        return <Tag text={tag} key={`card_tag_${index}`} />
+      })}
     </div>
-    <img src={item.imagemUrl}/>
+
+    <img src={item.imagemUrl} />
   </div>
 }
